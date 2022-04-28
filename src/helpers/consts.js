3fd5fefa49f86efcd21b6a0abc6538ee0a82ca1e
aizirekashymzhanova <96565067+aizirekashymzhanova@users.noreply.github.com>
2022-04-28 17:56:39 +0600

    the end

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,6 +1,10 @@
-export const API = "http://localhost:8000/products";
-export const API_COM = "http://localhost:8000/comments";
-export const API_LIKE = "http://localhost:8000/likes";
+// export const API = "http://localhost:8000/products";
+// export const API_COM = "http://localhost:8000/comments";
+// export const API_LIKE = "http://localhost:8000/likes";
+
+export const API = "https://english-zone.herokuapp.com/products";
+export const API_COM = "https://english-zone.herokuapp.com/comments";
+export const API_LIKE = "https://english-zone.herokuapp.com/likes";
 
 export const ACTIONS = {
   GET_PRODUCTS: "GET_PRODUCTS",
